d7fa62d668e98ca177489cd2cf0dd9bbe0d4f0f2
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-06 16:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+      if(sampleObject.hasOwnProperty() === key)
+	  {
+		  return false;
+	  } else{
+		  return true;
+	  }
 }
 
 // Do not change the code below
