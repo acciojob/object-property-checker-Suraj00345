a175affc07e660088f2d3636b0cf736d3183b2b1
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-06 17:23:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-     return key in sampleObject;
-}
-const keyToCheck = "green";
-const keyExists = hasKey(keyToCheck);
-
-if (keyExists) {
- return true;
-} else {
-  return false;
+    if(sampleObject.hasOwnProperty(key))
+	{
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // Do not change the code below
-// const key = prompt("Enter Key.");
-// alert(hasKey(key));
+const key = prompt("Enter Key.");
+alert(hasKey(key));
